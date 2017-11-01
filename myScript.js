43c4416083df31864dab0fb451ca5832c2a5f743
stognienkovladislav <stognienkovv@gmail.com>
2017-11-01 18:11:56 +0200

    toolbar

--- myScript.js
+++ myScript.js
@@ -1,2 +1,11 @@
 
-chrome.runtime.sendMessage(document.getElementsByTagName('title')[0].innerText);
+var url = chrome.extension.getURL('toolbar.html');
+var height = "35px";
+var iframe = "<iframe src ='"+url+"' id = 'myOwnCustomFirstToolbar' style='height:"+height+"'></iframe>";
+
+
+$('html').append(iframe);
+
+$('body').css({
+   '-webkit-transform' : 'translateY('+height+')'
+});
