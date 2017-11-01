530fc94dfcb639ddfd8f7f42233c4cd949d37a47
stognienkovladislav <stognienkovv@gmail.com>
2017-11-01 19:15:43 +0200

    diffrent links

--- Toolkit/background.js
+++ Toolkit/background.js
@@ -1,13 +1,36 @@
 
-chrome.contextMenus.create({
+var contextsList = ["selection", "link", "image", "page"];
 
-   title: " Twitter Social Toolkit",
-   contexts:["selection"],
-   onclick: myFunction
+for (i = 0; i < contextsList.length; i++) {
+    var context = contextsList[i];
+    var titleX = "Twitter toolkit: Share this " + context + " on your twitter profile";
 
-});
+    chrome.contextMenus.create({
 
+        title: titleX,
+        contexts: [context],
+        onclick: myFunction,
+        id: context
 
-function myFunction() {
-    alert("You just clicked me folk!")
+    });
 }
+
+function myFunction(data) {
+    switch(data.menuItemId){
+        case 'selection' :
+            chrome.tabs.create({url: "https://twitter.com/intent/tweet?text="+data.selectionText});
+            break;
+
+        case 'link':
+            chrome.tabs.create({url: "https://twitter.com/intent/tweet?url="+data.linkUrl});
+            break;
+
+        case 'image':
+            chrome.tabs.create({url: "https://twitter.com/intent/tweet?url="+data.srcUrl});
+            break;
+
+        case 'page':
+            chrome.tabs.create({url: "https://twitter.com/intent/tweet?text=MyPage"});
+            break;
+    }
+    }
