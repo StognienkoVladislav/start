374978a6ee4bce9e9db26a937df7edd1406bdf57
stognienkovladislav <stognienkovv@gmail.com>
2017-11-13 23:33:06 +0200

    Save and get if plugin menu

--- Extensions/Storage/script.js
+++ Extensions/Storage/script.js
@@ -1,6 +1,17 @@
 window.onload = function () {
     document.getElementById("save").onclick=function () {
       var value = document.getElementById("saveLine").value;
-      alert(value);
+      //alert(value);
+
+      chrome.storage.sync.set({"myLine":value}, function(){
+          alert("success!");
+      });
+
+    };
+
+    document.getElementById("get").onclick = function () {
+      chrome.storage.sync.get('myLine', function (data) {
+          alert(data.myLine);
+      });
     };
 };
